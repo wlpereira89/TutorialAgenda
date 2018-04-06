4373bde59ac2cba4abe880e294cd07beb4ac53c5
Wagner L. Pereira <wagnerluis1989@gmail.com>
2018-04-06 11:54:54 -0300

    Agenda 2 - Criação da Database

---
+++ AgendaDB/Contatos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Contatos]
+(
+	[Email] VARCHAR(50) NOT NULL , 
+    [TipoContato] INT NOT NULL, 
+    [telefone] NCHAR(16) NULL, 
+    PRIMARY KEY ([Email], [TipoContato])
+)
---
+++ AgendaDB/Pessoas.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Pessoas]
+(
+	[Email] VARCHAR(50) NOT NULL PRIMARY KEY, 
+    [Nome] VARCHAR(50) NULL, 
+    [Idade] INT NULL
+)
