3770cd92948c1ba0e95338b5eef8a9c3bc4a60dc
Wagner L. Pereira <wagnerluis1989@gmail.com>
2018-04-06 12:15:36 -0300

    Agenda 3 - Criação doDAO Ado Modelo e PN Agenda e configuração das strings de conexação em todo projeto

---
+++ Modelo/PN/pnAgenda.cs
@@ -0,0 +1,70 @@
+﻿using Modelo.DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo.PN
+{
+    public static class pnAgenda
+    {
+        public static bool Inserir (Pessoa p)
+        {
+            try
+            {
+                AgendaEntities db = new AgendaEntities();
+
+                db.Pessoas.Add(p);
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static bool Alterar(Pessoa p)
+        {
+            try
+            {
+                AgendaEntities db = new AgendaEntities();
+                Pessoa pa = new Pessoa();
+
+                pa = db.Pessoas.Find(p.Email);
+
+                pa.Idade = p.Idade;
+                pa.Nome = p.Nome;
+
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+        public static bool Excluir(Pessoa p)
+        {
+            try
+            {
+                AgendaEntities db = new AgendaEntities();
+                Pessoa pa = new Pessoa();
+
+                pa = db.Pessoas.Find(p.Email);
+
+                db.Pessoas.Remove(pa);
+
+                db.SaveChanges();
+
+                return true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+    }
+}
