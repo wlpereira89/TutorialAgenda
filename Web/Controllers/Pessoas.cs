95c8c393ae7ce109ddf8544d8ee84286382d469a
Wagner L. Pereira <wagnerluis1989@gmail.com>
2018-04-06 15:35:39 -0300

    ajustes nas referencias ao modelo na web e desk e tutorial Agenda 7 criando um CRUD para Pessoa

---
+++ Web/Controllers/Pessoas.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Modelo.DAO;
+
+namespace Web.Controllers
+{
+    public class Pessoas : Controller
+    {
+        private AgendaEntities db = new AgendaEntities();
+
+        // GET: Pessoas
+        public ActionResult Index()
+        {
+            return View(db.Pessoas.ToList());
+        }
+
+        // GET: Pessoas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = db.Pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pessoas/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Email,Nome,Idade")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pessoas.Add(pessoa);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = db.Pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoas/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Email,Nome,Idade")] Pessoa pessoa)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pessoa).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pessoa);
+        }
+
+        // GET: Pessoas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pessoa pessoa = db.Pessoas.Find(id);
+            if (pessoa == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pessoa);
+        }
+
+        // POST: Pessoas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            Pessoa pessoa = db.Pessoas.Find(id);
+            db.Pessoas.Remove(pessoa);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
